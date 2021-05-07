f2ac72e759f6ad72b18ba3ecab49fd682fff3b5b
Alessandro Marega <ale_marega@hotmail.it>
2021-05-07 16:48:38 +0200

    aggiunto header e variabili scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,27 @@
 <header>
-  
+  <div class="container">
+    {{-- logo --}}
+    <div class="logo_container">
+      <img class="mrg_up_down_40" src="{{ asset('img/marchio-sito-test.png')}}" alt="Logo Marchio">
+    </div>
+    {{-- /logo --}}
+
+    {{-- main_nav --}}
+    <div class="main_nav">
+      <ul class="flex">
+        <li>
+          <a class="active" href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Prodotti</a>
+        </li>
+        <li>
+          <a href="#">News</a>
+        </li>
+      </ul>
+    </div>
+    {{-- /main_nav --}}
+
+
+  </div>
 </header>
