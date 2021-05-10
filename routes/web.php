f755daa9e58e1a53a92e111bfc08520261aae1b8
Alessandro Marega <ale_marega@hotmail.it>
2021-05-10 17:16:23 +0200

    aggiunto controllo su query string prodotti

--- routes/web.php
+++ routes/web.php
@@ -288,13 +288,18 @@ Route::get('/prodotto/{id}', function ($id) {
             "descrizione" => "Altro elemento cult della famiglia de lo Spaghetto Quadrato (N.1 Spaghetto Quadrato. Una new entry che sarà molto apprezzata sia dai consumatori che dagli chef, perché il Ditale Quadrato è un formato deliziosamente piccolo ma sostanzioso.<br>A dispetto del nome che fa pensare ad una pastina è un formato di pasta assolutamente versatile, adatto a moltissime ricette di primi piatti.<br>La sua consistenza soda si sprigiona in bocca con un\'esplosione di emozioni, grazie agli spessori corposi, al colore elegantemente ambrato, alla texture delicatamente ruvida, cangiante e piacevolissima al tatto che trattiene il condimento sulla superficie.<br>Il Ditale Quadrato sembra ideale per preparazioni strutturate come la ricetta con crema di broccoletto siciliano, calamari e pomodori semi secchi profumata al limone e carbone d\'olive nere."
         ]
     ];
-    // ciclo e creo array di tipi di pasta
-    foreach ($data as $key => $el) {
-       
-        // id elemento corrispondente indice del data
-        $el["id"] = $key;
 
-        $data[$key] = $el;
+    if (intval($id) > (count($data) - 1)) {
+        abort(404);
+    } else {
+        // ciclo e creo array di tipi di pasta
+        foreach ($data as $key => $el) {
+        
+            // id elemento corrispondente indice del data
+            $el["id"] = $key;
+
+            $data[$key] = $el;
+        }
     }
 
     return view('/pages/product', [
